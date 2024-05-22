1a1292626b5c39481d196799d3c527bea2435f16
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-05-22 14:32:39 +0700

    build: add spring boot and other depedencie

---
+++ src/main/java/com/wreckitral/DoYourAssignment/DoYourAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.wreckitral.DoYourAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoYourAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoYourAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wreckitral/DoYourAssignment/DoYourAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wreckitral.DoYourAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoYourAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
