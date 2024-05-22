7ef6c017b5ef11113fce607791a7d01d998d6db6
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-05-22 15:12:52 +0700

    feat: create model for todo item

---
+++ src/main/java/com/wreckitral/DoYourAssignment/model/TodoItem.java
@@ -0,0 +1,37 @@
+package com.wreckitral.DoYourAssignment.model;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "todo_items")
+public class TodoItem implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String description;
+
+    private Boolean isComplete;
+
+    private Instant createdAt;
+
+    private Instant updateAt;
+
+    @Override
+    public String toString() {
+        return String.format("TodoItem{id=%d, description='%s', isComplete='%s', createdAt='%s', updateAt='%s'}", 
+            id, description, isComplete, createdAt, updateAt);
+    }
+}
